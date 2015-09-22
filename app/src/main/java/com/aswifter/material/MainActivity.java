c69290e6de6a6893a09adce2520e181aff34b9b1
AlwaysBeMySelf <nht.nit.net@gmail.com>
2015-09-22 23:33:27 +0800

    Update MainActivity.java

--- app/src/main/java/com/aswifter/material/MainActivity.java
+++ app/src/main/java/com/aswifter/material/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements BackHandledFragme
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("MainActivity","GitTestThroughLocal");
+        Log.i("MainActivity","GitTestThroughWeb");
 
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
